d0de2875d7a30b8b8ece901e972b3ce854cebd2e
andreapmacedo <aa.andremacedo@gmail.com>
2022-08-18 10:45:09 -0300

    feito: aplicação com nodemon

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
 })
 
-
 app.listen(3001, () => {
   console.log('Servidor foi iniciado');
 })
+
+
+// npm install nodemon --save-dev
+// npx nodemon app.js
+
+// com script
+// npm dev
