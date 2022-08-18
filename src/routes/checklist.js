79cf60f0a1625acf18d6ff94293f792c97a0f122
andreapmacedo <aa.andremacedo@gmail.com>
2022-08-18 12:59:23 -0300

    feito: aplicação com rotas POST

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,15 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-// forma 1
-// router.get('/checklists', (req, res) => {
-//   console.log('Olá');
-//   res.send();
-// })
-// forma 2
-router.get('/', (req, res) => {
-  console.log('Olá');
-  res.send();
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.status(200).send(req.body);
+  // res.status(200).json(req.body); // enviando no formato json
 })
-
 module.exports = router;
