2683ac474ee8a549c10eea038bfd48a4fa948bb5
andreapmacedo <aa.andremacedo@gmail.com>
2022-08-18 13:20:57 -0300

    feito: aplicação com put e delete

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,10 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/:id', (req, res) => {
-  console.log(req.params);
-  console.log(req.params.id);
-  res.send(`ID: ${req.params.id}`);
+router.put('/:id', (req, res) => {
+  console.log(req.body);
+  res.send(`PUT ID: ${req.params.id}`);
 })
+
+router.delete('/:id', (req, res) => {
+  console.log(req.body);
+  res.send(`DELETE ID: ${req.params.id}`);
+})
+
 
 module.exports = router;
